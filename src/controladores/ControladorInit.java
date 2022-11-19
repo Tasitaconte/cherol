a158e1a2caab09f65c399786d3c86507f886dd0a
tasita <thomasmauricio521@gmail.com>
2022-11-18 19:13:27 -0500

    actualizaciones

---
+++ src/controladores/ControladorInit.java
@@ -0,0 +1,14 @@
+package controladores;
+
+import vistas.VistaIngreso;
+
+public class ControladorInit {
+    
+    static VistaIngreso v = new VistaIngreso();
+    
+    public static void visible(){
+        v.setVisible(true);
+    }
+    
+    
+}
